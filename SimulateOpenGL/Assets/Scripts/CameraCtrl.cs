b22a8ff8d9f3aed1e3626840bca6c7a2c9e9bcad
zhyingkun <1464959348@qq.com>
2019-03-11 18:19:50 +0800

    For Unity Proj

---
+++ SimulateOpenGL/Assets/Scripts/CameraCtrl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+
+	public float speed = 0.1f;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		this.processView();
+		this.processMove();
+	}
+	void processView(){
+		
+	}
+	void processMove()
+	{
+		Vector3 currentPosition = this.transform.position;
+		Vector3 delta = new Vector3(0.0f, 0.0f, 0.0f);
+		if (Input.GetKey(KeyCode.A))
+		{
+			delta.x = -1 * speed;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			delta.x = speed;
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			delta.z = speed;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			delta.z = -1 * speed;
+		}
+		this.transform.position = currentPosition + delta;
+
+	}
+}
